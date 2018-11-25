2fa9d6a8c5f21d85f893d2a51e588a464eef81d2
yuchunwang <1228368500@qq.com>
2018-11-25 22:11:04 +0800

    123

---
+++ SpringBootMyBatisRiJi/src/main/java/com/ycw/rgbt/RgbtApplication.java
@@ -0,0 +1,12 @@
+package com.ycw.rgbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgbtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RgbtApplication.class, args);
+    }
+}
--- SpringBootMyBatisRiJi/src/test/java/com/ycw/rgbt/RgbtApplicationTests.java
+++ SpringBootMyBatisRiJi/src/test/java/com/ycw/rgbt/RgbtApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ycw;
+package com.ycw.rgbt;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootMyBatisRiJiApplicationTests {
+public class RgbtApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
